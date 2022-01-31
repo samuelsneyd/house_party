1e604d93446f14d45793feb5a8608f70df5d8abb
Samuel <samuelsneyd@gmail.com>
2022-02-01 01:18:35 +1300

    Initialize react frontend.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./static/src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
@@ -25,7 +25,7 @@ module.exports = {
     new webpack.DefinePlugin({
       "process.env": {
         // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("production"),
+        "process.env.NODE_ENV": JSON.stringify("production"),
       },
     }),
   ],
