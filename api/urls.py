3c5de4651e3c913e3f3d5599943a666414d9f175
Samuel <samuelsneyd@gmail.com>
2022-01-31 23:26:47 +1300

    Add endpoint to list rooms.
    Add serializer.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import main
+from api.views import main, RoomView
 
 urlpatterns = [
-    path("", main),
+    path("home", RoomView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics
+from api.serializers import RoomSerializer
+from api.models import Room
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
 
 def main(request):
-    return HttpResponse("test")
+    pass
