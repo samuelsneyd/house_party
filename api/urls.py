84147ff3efac208b2abb547c55ae8209dd5efb19
Samuel <samuelsneyd@gmail.com>
2022-02-01 19:09:58 +1300

    Implement create room API backend

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import main, RoomView
+from api.views import RoomView, CreateRoomView
 
 urlpatterns = [
     path("room", RoomView.as_view()),
+    path("create", CreateRoomView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
-from api.serializers import RoomSerializer
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.serializers import RoomSerializer, CreateRoomSerializer
 from api.models import Room
 
 
@@ -8,5 +10,31 @@ class RoomView(generics.ListAPIView):
     serializer_class = RoomSerializer
 
 
-def main(request):
-    pass
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get("guest_can_pause")
+            votes_to_skip = serializer.data.get("votes_to_skip")
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=["guest_can_pause", "votes_to_skip"])
+            else:
+                room = Room(
+                    host=host,
+                    guest_can_pause=guest_can_pause,
+                    votes_to_skip=votes_to_skip,
+                )
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
