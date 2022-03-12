b61b44eac6ff6f937c45851f83c8772b3bb46a50
Samuel <samuelsneyd@gmail.com>
2022-03-12 17:11:29 +1300

    Begin join room page component

--- frontend/static/src/components/JoinRoomPage.js
+++ frontend/static/src/components/JoinRoomPage.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+
 
 export default function JoinRoomPage() {
+  const [roomCode, setRoomCode] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleTextFieldChange = (e) => {
+    setRoomCode(e.target.value.toUpperCase());
+  };
+
+  const enterRoom = () => {
+    console.log(roomCode);
+  };
+
   return (
-    <p>This is the join room page</p>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <TextField
+          error={!!error}
+          label="Code"
+          placeholder="Enter a room code"
+          value={roomCode}
+          helperText={error}
+          variant="outlined"
+          onChange={handleTextFieldChange}
+        />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={enterRoom}
+        >
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          color="secondary"
+          variant="contained"
+          to="/"
+          component={Link}
+        >
+          Back
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
