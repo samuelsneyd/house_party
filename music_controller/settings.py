fcfa515390b80f200426127535bc95f6e455f4a6
Samuel <samuelsneyd@gmail.com>
2022-03-13 20:42:48 +1300

    Initialize spotify app Add .env to settings

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Initialize environment variables
+
+env = environ.Env()
+environ.Env.read_env()
+
+SPOTIFY = {
+    "CLIENT_ID": env("SPOTIFY_CLIENT_ID"),
+    "CLIENT_SECRET": env("SPOTIFY_CLIENT_SECRET"),
+    "REDIRECT_URI": "",
+}
