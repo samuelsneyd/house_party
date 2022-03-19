aaa1163fa39fdc1897349ca455ada2fafe08f00e
Samuel <samuelsneyd@gmail.com>
2022-03-19 17:44:26 +1300

    Add path and view for spotify authentication

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from frontend import views
 
+app_name = "frontend"
+
 urlpatterns = [
-    path("", views.index),
-    path("join", views.index),
-    path("create", views.index),
+    path("", views.index, name=""),
+    path("join", views.index, name="join"),
+    path("create", views.index, name="create"),
     path("room/<str:roomCode>", views.index),
-    path("room", views.index),
+    path("room", views.index, name="room"),
 ]
